43a71d8cd43fcf760a9781b0c5a850f140b0d2c1
InstFisica <InstFisica@172.16.30.37>
2016-08-16 16:54:41 -0500

    punto8

---
+++ src/punto8/Punto8.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto8;
+import java.util.Scanner;
+
+/**
+ *
+ * @author InstFisica
+ */
+public class Punto8 {
+
+   
+    
+            
+    public static void main(String[] args) {
+        
+        
+       Scanner numero = new Scanner(System.in);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
